c9c5d019bf4339a23527acfefde17e47db306cdd
CUC <nada>
2024-05-06 11:54:24 -0500

    Read

--- src/app/components/peliculas/PeliListado.tsx
+++ src/app/components/peliculas/PeliListado.tsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+import { Pelicula } from "../../models/Pelicula";
+import { ARREGLO_PELICULA } from "../../mocks/Pelicula-mocks";
+import { ARREGLO_PELICULA_GENERO } from "../../utility/dominios/DomGenero";
+
 export const PeliListado = () => {
+  const [arrPeliculas] = useState<Pelicula[]>(ARREGLO_PELICULA);
+
+  const obtenerNombreGenero = (valor: string) => {
+    for (const objGen of ARREGLO_PELICULA_GENERO) {
+      if (objGen.codigo == valor) {
+        return objGen.nombreGenero;
+      }
+    }
+  };
+
   return (
     <>
       <div className="d-flex justify-content-center">
@@ -10,94 +25,22 @@ export const PeliListado = () => {
                 <th style={{ width: "30%" }}>Nombre</th>
                 <th style={{ width: "20%" }}>Genero</th>
                 <th style={{ width: "30%" }}>Protagonista</th>
-                <th style={{ width: "10%" }}> </th>
+                <th style={{ width: "10%" }}>Imagen</th>
               </tr>
             </thead>
             <tbody>
-              <tr className="align-middle">
-                <td>1</td>
-                <td>Top Gun</td>
-                <td>1</td>
-                <td>Tom Cruise</td>
-                <td className="text-center">
-                  <div>
-                    <a href="/#">
-                      <img alt="" className="imagenListado" />
-                    </a>
-                    <br />
-                    <small>fotico.png</small>
-                  </div>
-                </td>
-              </tr>
-              <tr className="align-middle">
-                <td>2</td>
-                <td>Jason vs Freddy</td>
-                <td>4</td>
-                <td>Jason krugeur</td>
-                <td className="text-center">
-                  <div>--</div>
-                </td>
-              </tr>
-              <tr className="align-middle">
-                <td>3</td>
-                <td>Dragon Ball GT</td>
-                <td>1</td>
-                <td>Goku</td>
-                <td className="text-center">
-                  <div>--</div>
-                </td>
-              </tr>
-              <tr className="align-middle">
-                <td>4</td>
-                <td>Matanza en Texas</td>
-                <td>2</td>
-                <td>KIll perro</td>
-                <td className="text-center">
-                  <div>
-                    <a href="/#">
-                      <img alt="" className="imagenListado" />
-                    </a>
-                    <br />
-                    <small>matanza.jpg</small>
-                  </div>
-                </td>
-              </tr>
-              <tr className="align-middle">
-                <td>5</td>
-                <td>Tiburón IV</td>
-                <td>3</td>
-                <td>El shark</td>
-                <td className="text-center">
-                  <div>--</div>
-                </td>
-              </tr>
-              <tr className="align-middle">
-                <td>6</td>
-                <td>Start trek</td>
-                <td>1</td>
-                <td>Capitan Kirk</td>
-                <td className="text-center">
-                  <div>--</div>
-                </td>
-              </tr>
-              <tr className="align-middle">
-                <td>7</td>
-                <td>Dioses del olimpo</td>
-                <td>2</td>
-                <td>Zeus dos</td>
-                <td className="text-center">
-                  <div>--</div>
-                </td>
-              </tr>
-              <tr className="align-middle">
-                <td>8</td>
-                <td>Un pistolero rudo</td>
-                <td>1</td>
-                <td>Balitas</td>
-                <td className="text-center">
-                  <div>--</div>
-                </td>
-              </tr>
+              {arrPeliculas.map((miPeli: Pelicula) => (
+                <tr key={miPeli.codPelicula}>
+                  <td>{miPeli.codPelicula} </td>
+                  <td>{miPeli.nombrePelicula}</td>
+                  <td>{obtenerNombreGenero(miPeli.generoPelicula)}</td>
+                  <td>{miPeli.protagonistaPelicula}</td>
+                  <td>
+                    <img src={miPeli.imagenPeliculaBase64} alt="" className="imagenListado" />
+                    <div className="">{miPeli.imagenPelicula}</div>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
